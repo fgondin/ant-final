4843dd7c6b4a6dbe2902ff42579a14a1855be290
Frederico Gondin <108304623+fgondin@users.noreply.github.com>
2023-09-19 15:53:46 -0300

    ant first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Task } from "./models/task.model";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  tasks: Task[] = [
+    {id: 0, descricao: 'Lavar a louça', status: false},
+    {id: 1, descricao: 'Passar o café', status: false},
+    {id: 2, descricao: 'Assistir aula', status: false}
+  ];
+
+  constructor(private alert: AlertController, 
+    private toast: ToastController) {}
+  //Constructor é um método especial que serve para inicializar os membros da classe.
+  //No angular, o construtor serve para injetar independências.
+
+  async showToast(message: string){ //Typescript funciona de forma dinânima, então precisamos usar o async para definir que a função espere algo.
+    const TOASTSCREEN = await this.toast.create({
+      //Usando o await para a função entender que ela pode ser finalmente chamada.
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      cssClass: 'toast-config'
+    });
+    TOASTSCREEN.present();
+  }
+
+  async updateAlert(){ 
+    const ALERTSCREEN = await this.alert.create({ //propriedade "alert" sendo declarada no constructor.
+      header: 'Update task?',
+      inputs: [
+        {
+        name: 'task',
+        type: 'text',
+        placeholder: 'New name...'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Update',
+          handler: (form) =>{
+            this.updateData(form);
+          }
+        }
+      ]
+    });
+
+    ALERTSCREEN.present //Para tornar o alerta visível, é necessário usar o present.
+  }
+
+  updateData(form: any){
+    if(!form.task || form.task.trim() == ''){
+      this.showToast('Please, enter a valid value');
+      return;
+    }
+  }
+}
+
+
---
+++ src/app/home/models/task.model.ts
@@ -0,0 +1,5 @@
+export interface Task {
+    id?: number;
+    descricao: string;
+    status: boolean;
+}
